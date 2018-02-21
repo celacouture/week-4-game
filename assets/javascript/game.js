5deae8326cc112becca41764432bf6d53de2512a
Calle Lacouture <celacouture@gmail.com>
2018-02-20 23:20:09 -0600

    week-4-game

---
+++ assets/javascript/game.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+var goalNumber;
+var gemNumber;
+var totalNumber;
+var wins=0
+var losses=0
+
+
+$(".btn").on('click', function(){
+    alert("I work");
+})
+
+$
+
+});
