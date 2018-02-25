a69358fdd74a63482635beb92b6b16eaaadfb0ca
Calle Lacouture <celacouture@gmail.com>
2018-02-24 20:48:27 -0600

    week-4-game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,18 @@
-$(document).ready(function(){
+//global variables
+var wins=0;
+var losses=0;
+var total=0;
+var targetNumber=Math.floor((Math.random()*100)+19);
+console.log(targetNumber);
 
-var goalNumber;
-var gemNumber;
-var totalNumber;
-var wins=0
-var losses=0
-
-
-$(".btn").on('click', function(){
-    alert("I work");
-})
-
-$
-
+var gemBlue=Math.floor((Math.random()*12)+1);
+var gemGreen=Math.floor((Math.random()*12)+1);
+var gemPink=Math.floor((Math.random()*12)+1);
+var gemGold=Math.floor((Math.random()*12)+1);
+console.log(gemBlue, gemGreen, gemPink, gemGold);
+//button clicks
+$( ".btn" ).click(function() {
+  alert( "works" );
 });
+
+//functions
